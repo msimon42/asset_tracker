2cb13ba686870bd1e2e48015a79df9221a8e1528
msimon42 <msimon@uab.edu>
2021-02-02 19:40:40 -0600

    Add connection

--- app/services/nomics_service.rb
+++ app/services/nomics_service.rb
@@ -1,5 +1,11 @@
-class NomicsService 
+class NomicsService
   include ServiceHelper
   private
 
+  def connection
+    Faraday.new(url: 'https://api.nomics.com/v1') do |f|
+        f.adapter Faraday.default_adapter
+        f.params[:key] = ENV['NOMICS_KEY']
+    end
+  end
 end
