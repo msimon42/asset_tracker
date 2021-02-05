7fff1b3354e1d182737c0491a0e97b51f7f5cdff
msimon42 <msimon@uab.edu>
2021-02-04 20:21:02 -0600

    Add connection and get_request

--- app/services/yf_service.rb
+++ app/services/yf_service.rb
@@ -1,4 +1,16 @@
 class YFService
 
+  private
+    def connection
+      Faraday.new(url: 'https://apidojo-yahoo-finance-v1.p.rapidapi.com') do
+        f.headers['x-rapidapi-key'] = ENV['YF_KEY']
+        f.headers['x-rapidapi-host'] = ENV['YF_HOST']
+        f.adapter Faraday.default_adapter
+      end
+    end
 
-end  
+    def get_request(endpoint, params=nil)
+      response = connection.get(endpoint, params)
+      return response.body
+    end
+end
