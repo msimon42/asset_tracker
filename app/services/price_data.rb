45fe041caa2c90d439a136344dfe1cb571a091d9
msimon42 <msimon@uab.edu>
2021-02-09 20:28:22 -0600

    Add stock quotes method

--- app/services/price_data.rb
+++ app/services/price_data.rb
@@ -1,6 +1,15 @@
 class PriceData
   def stock_quotes(tickers)
-
+    yf = YfService.new
+    quotes = tickers.map do |ticker|
+      data = yf.get_individual_stock(ticker)
+      begin
+        Quote.new(ticker, data['price']['regularMarketPrice']['raw'])
+      rescue
+        next
+      end
+    end
+    binding.pry
   end
 
   def crypto_quotes(tickers)
